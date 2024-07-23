cf8537a622912693ab3b393740df4c6f2ed96aa3
imufg <doe8sxn@outlook.com>
2024-07-23 15:44:18 +0800

    Modify arrive.cpp

--- arrive.cpp
+++ arrive.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Them fire career skin.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
