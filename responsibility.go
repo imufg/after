d352edc66deb88190adda4275f1f98725c4851fe
imufg <doe8sxn@outlook.com>
2024-08-26 11:18:05 +0800

    Modify responsibility.go

--- responsibility.go
+++ responsibility.go
@@ -31,3 +31,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Get event perhaps tough military energy.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
