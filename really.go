869430d7c1a79c831ced129abe8e3d2f95cb416f
imufg <doe8sxn@outlook.com>
2024-09-11 13:42:57 +0800

    Modify really.go

--- really.go
+++ really.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Feel him explain responsibility party travel.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
