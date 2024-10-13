0c261115486b809788c7ac0d3024671873fe993e
imufg <doe8sxn@outlook.com>
2024-10-13 12:02:47 +0800

    Modify less.cpp

--- less.cpp
+++ less.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Participant president like moment pull girl ground.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
