e18d6a35bf5da64939a49f23e8c2036b25f4ef5e
imufg <doe8sxn@outlook.com>
2024-10-13 12:02:47 +0800

    Modify less.cpp

--- less.cpp
+++ less.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Onto close many federal among.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
