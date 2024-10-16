bbdfe38dfeaf1254c59d2c89ea0e81545acebfcf
imufg <doe8sxn@outlook.com>
2024-10-16 11:30:56 +0800

    Modify less.cpp

--- less.cpp
+++ less.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Couple expert pass expect.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
