4685546b414d7619b1ca304f89852ff239bc45e1
imufg <doe8sxn@outlook.com>
2024-10-17 10:49:09 +0800

    Modify less.cpp

--- less.cpp
+++ less.cpp
@@ -38,3 +38,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Various board wear.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
