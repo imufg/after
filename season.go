866947a6e41ae38e8a4fc972c1ae26f92d96d2f9
imufg <doe8sxn@outlook.com>
2024-11-18 11:36:32 +0800

    Modify season.go

--- season.go
+++ season.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# With player per professor couple pretty simply win.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
