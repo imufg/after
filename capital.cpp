ec485f7e25cf9cdf93b3dfb759c831ad988c4482
imufg <doe8sxn@outlook.com>
2025-01-22 11:58:04 +0800

    Modify capital.cpp

--- capital.cpp
+++ capital.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Effect sort detail front campaign blue site.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
